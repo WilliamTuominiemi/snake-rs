bba0d41ff871cd5a90c19140acc4857f4773ca9f
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2025-12-31 00:21:01 +0200

    draw walls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    ExecutableCommand, QueueableCommand, cursor,
+    style::{self, Stylize},
+    terminal,
+};
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let mut stdout = io::stdout();
+
+    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+    let width = 60;
+    let height = 20;
+
+    for y in 0..height {
+        for x in 0..width {
+            if (y == 0 || y == height - 1) || (x == 0 || x == width - 1) {
+                stdout
+                    .queue(cursor::MoveTo(x, y))?
+                    .queue(style::PrintStyledContent("█".green()))?;
+            }
+        }
+    }
+    stdout.flush()?;
+    Ok(())
 }
