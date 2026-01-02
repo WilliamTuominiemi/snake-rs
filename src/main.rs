e39980e5d5db2ac87e3ede344575f3f5c765ba08
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2026-01-03 01:29:35 +0200

    game update loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,57 @@
 use crossterm::{
     ExecutableCommand, QueueableCommand, cursor,
+    event::{self, Event, KeyCode, KeyEvent},
     style::{self, Stylize},
     terminal,
 };
 use std::io::{self, Write};
+use std::time::Duration;
+
+struct Game {
+    refresh_rate: u8,
+    quit: bool,
+}
+
+impl Game {
+    pub fn new(refresh_rate: u8) -> Self {
+        Self {
+            refresh_rate,
+            quit: false,
+        }
+    }
+
+    pub fn update(&mut self) -> io::Result<()> {
+        terminal::enable_raw_mode();
+
+        let mut stdout = io::stdout();
+
+        while !self.quit {
+            if event::poll(Duration::from_millis(100))? {
+                if let Event::Key(key_event) = event::read()? {
+                    match key_event.code {
+                        KeyCode::Char('q') => self.quit = true,
+                        _ => {}
+                    }
+                }
+            }
+
+            stdout
+                .execute(terminal::Clear(terminal::ClearType::All))?
+                .execute(cursor::MoveTo(0, 0))?;
+            println!("test");
+
+            let width = 20;
+            let height = 10;
+
+            draw(draw_walls(&mut stdout, width, height));
+
+            stdout.flush()?;
+        }
+
+        terminal::disable_raw_mode()?;
+        Ok(())
+    }
+}
 
 fn draw_walls(
     stdout: &mut io::Stdout,
@@ -31,15 +79,7 @@ fn draw(result: Result<(), Box<dyn std::error::Error>>) {
 }
 
 fn main() -> io::Result<()> {
-    let mut stdout = io::stdout();
-
-    stdout.execute(terminal::Clear(terminal::ClearType::All))?;
-
-    let width = 60;
-    let height = 20;
-
-    draw(draw_walls(&mut stdout, width, height));
-
-    stdout.flush()?;
+    let mut game = Game::new(60);
+    game.update()?;
     Ok(())
 }
