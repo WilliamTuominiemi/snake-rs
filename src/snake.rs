a4d955ea9a7cc50d19cbf9bad84d376b0b6e866f
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2026-01-05 16:42:52 +0200

    unit tests for snake

--- src/snake.rs
+++ src/snake.rs
@@ -42,3 +42,49 @@ impl Snake {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_updating_position() {
+        let start_x = 5;
+        let start_y = 5;
+
+        let mut snake = Snake::new(start_x, start_y); // Start direction is right
+
+        snake.update_position();
+
+        let (x, y) = snake.position();
+
+        assert_eq!(x, start_x + 1);
+        assert_eq!(y, start_y);
+    }
+
+    #[test]
+    fn test_keeping_within_bounds() {
+        let start_x = 5;
+        let start_y = 5;
+
+        let width = 20;
+        let height = 10;
+
+        let mut snake_within_bounds = Snake::new(start_x, start_y);
+        snake_within_bounds.keep_within_bounds(width, height);
+        let (x, y) = snake_within_bounds.position();
+
+        assert_eq!(start_x, x);
+        assert_eq!(start_y, y);
+
+        let start_x_outside = width;
+        let start_y_outside = height;
+
+        let mut snake_outside_bounds = Snake::new(start_x_outside, start_y_outside);
+        snake_outside_bounds.keep_within_bounds(width, height);
+        let (x, y) = snake_outside_bounds.position();
+
+        assert_eq!(0, x);
+        assert_eq!(0, y);
+    }
+}
